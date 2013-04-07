239c3c1243f970ed6b8138e7a28f268baab7df33
Iñaki Elcoro <ielcoro@consultec.es>
2013-04-07 02:44:55 +0200

    Added projects

---
+++ xUnitTypedClassData/src/xunit.extensions.typedclassdata/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("xunit.extensions.typedclassdata")]
+[assembly: AssemblyDescription("xUnit Extension that allows to use typed classes for data theories")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Pistatxo Team")]
+[assembly: AssemblyProduct("xUnit Typed Class Data")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("Pistatxo Team")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM. Si necesita obtener acceso a un tipo de este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de typelib si este proyecto se expone a COM
+[assembly: Guid("4ab7c400-9b22-4082-895a-6585cdaf8afe")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o establecer como predeterminados los números de compilación y de revisión 
+// mediante el carácter '*', como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
