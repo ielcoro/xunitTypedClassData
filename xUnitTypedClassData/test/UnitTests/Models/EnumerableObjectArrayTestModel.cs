534f3318ea7c95996e11d98d4194118c2135253f
Iñaki Elcoro <ielcoro@consultec.es>
2013-04-07 02:59:29 +0200

    ShouldBeCompatibleWithObjectArrayEnumerables Passed

---
+++ xUnitTypedClassData/test/UnitTests/Models/EnumerableObjectArrayTestModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit.Extensions;
+
+namespace UnitTests.Models
+{
+    public class EnumerableObjectArrayTestModel
+    {
+        [Theory, ClassData(typeof(EnumerableObjectArrayData))]
+        public void EnumerableTest(int foo, string bar)
+        {
+
+        }
+    }
+
+    public class EnumerableObjectArrayData : IEnumerable<object[]>
+    {
+
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1, "bar" };
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ xUnitTypedClassData/test/UnitTests/TypedClassDataTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTests.Models;
+using Xunit;
+using Xunit.Sdk;
+
+namespace UnitTests
+{
+    public class TypedClassDataTests
+    {
+        private IEnumerable<MethodResult> RunClass(Type typeUnderTest)
+        {
+            ITestClassCommand testClassCommand = new TestClassCommand(typeUnderTest);
+
+            ClassResult classResult = TestClassCommandRunner.Execute(testClassCommand, testClassCommand.EnumerateTestMethods().ToList(),
+                                                                     startCallback: null, resultCallback: null);
+
+            return classResult.Results.OfType<MethodResult>();
+        }
+
+        [Fact]
+        public void ShouldBeCompatibleWithObjectArrayEnumerables()
+        {
+            MethodResult testResult = RunClass(typeof(EnumerableObjectArrayTestModel)).Single();
+
+            Assert.IsType<PassedResult>(testResult);
+            Assert.Equal(@"UnitTests.Models.EnumerableObjectArrayTestModel.EnumerableTest(foo: 1, bar: ""bar"")", testResult.DisplayName);
+        }
+
+    }
+}
